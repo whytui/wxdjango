a682ef6222ed30fde05828d1f9bc4f9536ddb08a
whytui <whytui@qq.com>
2022-12-24 21:08:55 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	wxcloudrun/templates/index.html

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from . import views
 # Register your models here.
-from blog.models import Article,Tag,Category
+from blog.models import Article, Tag, Category
 
-admin.site.register(Article)
+
 admin.site.register(Tag)
 admin.site.register(Category)
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category', 'title', 'content', 'description', 'img',)
--- blog/models.py
+++ blog/models.py
@@ -26,13 +26,15 @@ class Tag(models.Model):
 
 class Article(models.Model):
     title = models.CharField('标题', max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, verbose_name="分类", blank=True, null=True)
+    img = models.ImageField(upload_to='article_img/%Y%m/%d', verbose_name="文章图片", blank=True, null=True)
+    description = models.TextField('描述',max_length=200)
     content = models.TextField('正文')
     pub_date = models.DateField('发布时间', auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, verbose_name="分类", blank=True, null=True)
     tags = models.ManyToManyField(Tag, related_name='articles', blank=True)
-    description = models.TextField('描述')
-    img = models.ImageField(upload_to='article_img/%Y%m/%d', verbose_name="文章图片", blank=True, null=True)
+
+
 
     def __str__(self):
         return self.title
